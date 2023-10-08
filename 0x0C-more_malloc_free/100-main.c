3f42458ea94c5de00d592ef821916825e33d7ded
leemcotech <adebayoteslim04@gmail.com>
2023-10-08 14:44:24 -0700

    100-realloc

--- 0x0C-more_malloc_free/100-main.c
+++ 0x0C-more_malloc_free/100-main.c
@@ -39,3 +39,16 @@ void simple_print_buffer(char *buffer, unsigned int size)
 int main(void)
 {
     char *p;
+    int i;
+
+    p = malloc(sizeof(char) * 10);
+    p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+    i = 0;
+    while (i < 98)
+    {
+        p[i++] = 98;
+    }
+    simple_print_buffer(p, 98);
+    free(p);
+    return (0);
+}

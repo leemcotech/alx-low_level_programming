f6dd462149c9b21aaea07e2ab70656b97801266e
leemcotech <adebayoteslim04@gmail.com>
2023-10-08 13:40:05 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,4 +13,3 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *ptr1;
 	char *old_ptr;
 	unsigned int i;
-}
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,29 +13,3 @@ int is_digit(char *s)
 
 	while (s[i])
 	{
-	if (s[i] < '0' || s[i] > '9')
-	return (0);
-	i++;
-	}
-	return (1);
-	}
-/**
-* _strlen - returns the length of a string
-* @s: string to evaluate
-* Return: the length of the string
-*/
-	int _strlen(char *s)
-	{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-	i++;
-	}
-	return (i);
-	}
-/**
-* errors - handles errors for main
-*/
-	void errors(void)
-	{
